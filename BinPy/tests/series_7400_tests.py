aefdef1bd6b0bdf4ff96be779ca6abe67fb25604
mohitgujarathi14 <mohitontherocks14@gmail.com>
2014-04-05 00:40:53 -0600

    Tests for IC 74147,74148,74154

--- BinPy/tests/series_7400_tests.py
+++ BinPy/tests/series_7400_tests.py
@@ -647,3 +647,29 @@ def test_IC_74138():
     n = {15: 1, 14: 1, 13: 1, 12: 1, 11: 1, 10: 0, 9: 1, 7: 1}
     if n != testIC.run():
         assert False
+        
+def test_IC_74148():
+    testIC = IC_74148()
+    m = {1:1,2:0,3:1,4:0,5:0,8:0,16:1,10:0,11:0,12:0,13:0}
+    testIC.setIC(m)
+    n= {9: 0, 6: 0, 14: 0, 7: 0, 15:1}
+    if n != testIC.run():
+        assert False
+
+def test_IC_74147():
+    testIC = IC_74147()
+    m = {1:1,2:0,3:1,4:1,5:1,8:0,16:1,10:0,11:0,12:0,13:0}
+    testIC.setIC(m)
+    n= {9: 0, 6: 1, 14: 0, 7: 1}
+    if n != testIC.run():
+        assert False
+        
+######## 24 pin ICs ########
+
+def test_IC_74154():
+    testIC = IC_74154()
+    m = {20:0, 21:0, 22:0, 23:0, 18:0 ,19:0 ,12:0 ,24:1 }
+    testIC.setIC(m)
+    n= {1: 0, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 10: 1, 11: 1, 13: 1, 14: 1, 15: 1, 16: 1, 17: 1}
+    if n != testIC.run():
+        assert False
